7203e51c9c554a50c9031dcd56fed56064bcb261
Daniel Cousineau <dcousineau@gmail.com>
2012-06-24 19:27:12 -0500

    Stab at the schedule

--- pages/schedule.html.php
+++ pages/schedule.html.php
@@ -14,11 +14,21 @@
             <?php foreach ($schedule as $hour => $timeslot): ?>
                 <?php
                     $time = strtotime($hour);
+                    if ($timeslot['end'])
+                        $end = strtotime($timeslot['end']);
+                    else
+                        $end = false;
                 ?>
                 <li>
+                <?php if (count($timeslot['talks']) > 0): ?>
                     <a href="#<?php echo $day . '-' . $hour; ?>">
-                    <?php echo date('g:iA', $time); ?> - <?php echo strtoupper($timeslot['type']); ?>
-                    </a>    
+                        <h3><?php echo date('g:i A', $time); ?><?php echo $end ? ' - ' . date('g:i A', $end) : '' ?></h3>
+                        <p><strong><?php echo strtoupper($timeslot['type']); ?></strong></p>
+                    </a>
+                <?php else: ?>
+                    <h3><?php echo date('g:i A', $time); ?><?php echo $end ? ' - ' . date('g:i A', $end) : '' ?></h3>
+                    <p><strong><?php echo strtoupper($timeslot['type']); ?></strong></p>
+                <?php endif; ?>
                 </li>
             <?php endforeach; ?>
             </ul>
@@ -33,14 +43,38 @@
 <?php foreach ($data['schedule'] as $day => $schedule): ?>
 <?php foreach ($schedule as $hour => $timeslot): ?>
 <?php
+    if (count($timeslot['talks']) == 0) continue;
+    
     $date = strtotime($day);
     $time = strtotime($hour);
+    if ($timeslot['end'])
+        $end = strtotime($timeslot['end']);
+    else
+        $end = false;
 ?>
 <div data-role="page" data-add-back-btn="true" id="<?php echo $day . '-' . $hour; ?>">
     <?php print $header; ?>
     <div data-role="content">
 
-        <h1><?php echo strtoupper($timeslot['type']); ?></h1>
+        <div class="time-heading">
+            <?php echo date('g:i A', $time); ?><?php echo $end ? ' - ' . date('g:i A', $end) : '' ?>
+        </div>
+        
+        <?php foreach ($timeslot['talks'] as $talkId): $talk = $data['talks'][$talkId]; ?>
+        <div class="session clearfix">
+            <hgroup>
+                <h2><?php echo $talk['title']; ?></h2>
+                <h3><?php echo $talk['presenter']; ?></h2>
+            </hgroup>
+            
+            <p><?php echo $talk['description']; ?></p>
+            
+            <div class="details">
+                <span class="room"><?php echo $talk['room']; ?></span>
+                <a href="<?php echo $talk['joindin']; ?>" class="joindin" rel="external" target="_blank"><?php echo $talk['joindin']; ?></a>
+            </div>
+        </div>
+        <?php endforeach; ?>
 
     </div>
 </div>
